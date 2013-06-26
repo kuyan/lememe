c97e35cc022fb95cbf427e6d4605c9a73792787d
kuyan <kuyanatan.nlao@gmail.com>
2013-06-25 19:24:58 -0700

    Added javascript compression with Slimit to prod. rendering.

--- build.py
+++ build.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import distutils.dir_util
+import glob
+import os.path
 import sys
 
 from staticjinja import Renderer
 
 BUILD_DIR = './build'
 STATIC_DIR = './static'
+
 
 def _init_renderer():
     distutils.dir_util.mkpath(BUILD_DIR)  # Create build directory
@@ -15,13 +18,30 @@ def _init_renderer():
 
     return renderer
 
+
 def build_devel(renderer):
     """Build Memecap for development in BUILD_DIR."""
     renderer.run(debug=True, use_reloader=True)
 
+
 def build_production(renderer):
     """Build Memecap for production and hosting on Github Pages."""
     renderer.run()
+    compress_js(os.path.join(BUILD_DIR, 'js'))
+
+
+def compress_js(path):
+    """Compress javascript files in `path` with Slimit."""
+    import slimit
+    files = glob.iglob(os.path.join(path, '*.js'))
+
+    for js in files:
+        with open(js, 'r+') as source:
+            compressed = slimit.minify(source.read(), mangle=True, mangle_toplevel=False)
+            source.seek(0)
+            source.write(compressed)
+            source.truncate()
+
 
 cmd_routes = {
     'devel': build_devel,
